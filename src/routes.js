6fd580ee2cc6ec74fc3f2e2a59f0cda30780f343
Patrick <padraigtoc@gmail.com>
2019-07-09 00:18:19 -0700

    Setting up project, created header and footer

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from './HighOrderComponents/Layout'
+
+const Routes = (props) => {
+  return (
+    <div>
+      <Layout>
+        second lines
+      </Layout>
+    </div>
+  )
+
+}
+
+export default Routes;
